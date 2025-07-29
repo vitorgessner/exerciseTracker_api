24e382725f30974d4162e845d174bae682c2c4b1
Vitor Gessner <gessnervgg@gmail.com>
2025-07-29 15:49:40 -0300

    Começando template do express e a rota index

---
+++ server.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
